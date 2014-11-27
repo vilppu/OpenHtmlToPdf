9a3ee828a141163ef31525ec6c143fa856d54100
vilppu <tsvilp@gmail.com>
2014-11-27 20:11:30 +0200

    Cleanup

---
+++ OpenHtmlToPdf/Assets/ConverterExecutable.cs
@@ -0,0 +1,89 @@
+using System;
+using System.IO;
+using System.Reflection;
+
+namespace OpenHtmlToPdf.Assets
+{
+    sealed class ConverterExecutable
+    {
+        private const string ConverterExecutableFilename = "OpenHtmlToPdf.WkHtmlToPdf.exe";
+
+        private ConverterExecutable()
+        {
+        }
+
+        public static ConverterExecutable Get()
+        {
+            var bundledFile = new ConverterExecutable();
+
+            bundledFile.CreateIfConverterExecutableDoesNotExist();
+
+            return bundledFile;
+        }
+
+        public string FullConverterExecutableFilename
+        {
+            get { return ResolveFullPathToConverterExecutableFile(); }
+        }
+
+        private void CreateIfConverterExecutableDoesNotExist()
+        {
+            if (!File.Exists(FullConverterExecutableFilename))
+                Create(GetConverterExecutableContent());
+        }
+
+        private static byte[] GetConverterExecutableContent()
+        {
+            using (var resourceStream = GetConverterExecutable())
+            {
+                var resource = new byte[resourceStream.Length];
+
+                resourceStream.Read(resource, 0, resource.Length);
+
+                return resource;
+            }
+        }
+
+        private static Stream GetConverterExecutable()
+        {
+            return Assembly.GetExecutingAssembly().GetManifestResourceStream("OpenHtmlToPdf.Assets.OpenHtmlToPdf.WkHtmlToPdf.exe");
+        }
+
+        private void Create(byte[] fileContent)
+        {
+            try
+            {
+                if (!Directory.Exists(BundledFilesDirectory()))
+                    Directory.CreateDirectory(BundledFilesDirectory());
+
+
+                using (var file = File.Open(FullConverterExecutableFilename, FileMode.Create))
+                {
+
+                    file.Write(fileContent, 0, fileContent.Length);
+                }
+            }
+            catch (IOException)
+            {
+            }
+        }
+
+        private static string ResolveFullPathToConverterExecutableFile()
+        {
+            return Path.Combine(BundledFilesDirectory(), ConverterExecutableFilename);
+        }
+
+        private static string BundledFilesDirectory()
+        {
+            return Path.Combine(Path.GetTempPath(), CurrentAssemblyNameAndVersion());
+        }
+
+        private static string CurrentAssemblyNameAndVersion()
+        {
+            return string.Format("{0}_{1}_{2}",
+                Assembly.GetExecutingAssembly().GetName().Name,
+                Assembly.GetExecutingAssembly().GetName().Version,
+                Environment.Is64BitProcess ? 64 : 32);
+        }
+    }
+}
---
+++ OpenHtmlToPdf/LengthConversion.cs
@@ -0,0 +1,50 @@
+﻿namespace OpenHtmlToPdf
+{
+    public static class LengthConversion
+    {
+        public static Length Millimeters(this double length)
+        {
+            return Length.Millimeters(length);
+        }
+
+        public static Length Centimeters(this double length)
+        {
+            return Length.Centimeters(length);
+        }
+
+        public static Length Inches(this double length)
+        {
+            return Length.Inches(length);
+        }
+
+        public static Length Millimeters(this float length)
+        {
+            return Length.Millimeters(length);
+        }
+
+        public static Length Centimeters(this float length)
+        {
+            return Length.Centimeters(length);
+        }
+
+        public static Length Inches(this float length)
+        {
+            return Length.Inches(length);
+        }
+
+        public static Length Millimeters(this int length)
+        {
+            return Length.Millimeters(length);
+        }
+
+        public static Length Centimeters(this int length)
+        {
+            return Length.Centimeters(length);
+        }
+
+        public static Length Inches(this int length)
+        {
+            return Length.Inches(length);
+        }
+    }
+}
